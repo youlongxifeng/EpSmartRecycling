b71cb0a791e3d83db9a753dc46fc189f8a18d1a1
youlongxifeng <xie774505257@163.com>
2018-09-20 19:04:52 +0800

    第九次提交，UI更新基本完成，往后添加高德地图

---
+++ app/src/main/java/cn/epsmart/recycling/device/ui/fragment/signin/SignInFragment.java
@@ -0,0 +1,34 @@
+package cn.epsmart.recycling.device.ui.fragment.signin;
+
+import android.view.View;
+
+import cn.epsmart.recycling.device.R;
+import cn.epsmart.recycling.device.base.BaseMvpFragment;
+import cn.epsmart.recycling.device.mvp.BasePresenter;
+
+/**
+ * @Author: Administrator
+ * @Time: 2018 2018/9/20 16:48
+ * @description: （登录页面）
+ */
+public class SignInFragment extends BaseMvpFragment {
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getlayoutId() {
+        return R.layout.activity_sigin_main;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initDate() {
+
+    }
+}
